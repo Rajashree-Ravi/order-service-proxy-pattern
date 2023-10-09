9991d155caafa4010eb67dc7fa1c7e984bdb7a58
Rajashree Ravi <rajashreeravi09@gmail.com>
2023-10-09 13:41:15 +0530

    Added zuul proxy with api discovery

--- src/main/java/com/ecommerce/orderservice/OrderServiceApplication.java
+++ src/main/java/com/ecommerce/orderservice/OrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.ecommerce.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.retry.annotation.EnableRetry;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableRetry
 public class OrderServiceApplication {
